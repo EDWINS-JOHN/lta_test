f66b374b83d29da37870e90ca956bc6eabde9184
Mamba-cita <edwinsjohn52@gmail.com>
2022-11-22 11:24:33 +0300

    headless

--- test/conftest.py
+++ test/conftest.py
@@ -9,21 +9,21 @@ from utilities.customLogger import LogGen
 
 service = Service(executable_path=ChromeDriverManager().install())
 
-# @pytest.fixture()
-# def driver():
-#     options = Options()
-#     options.add_argument('--disable-blink-features=AutomationControlled')
-#     options.add_argument('headless')
-#     options.add_argument("start-maximized")
-#     driver = webdriver.Chrome(service=service, options=options)
-#     return driver
-
 @pytest.fixture()
 def driver():
-    chromedriver_autoinstaller.install()
-    driver = webdriver.Chrome(service=Service())
-    driver.maximize_window()
+    options = Options()
+    options.add_argument('--disable-blink-features=AutomationControlled')
+    options.add_argument('headless')
+    options.add_argument("start-maximized")
+    driver = webdriver.Chrome(service=service, options=options)
     return driver
+
+# @pytest.fixture()
+# def driver():
+#     chromedriver_autoinstaller.install()
+#     driver = webdriver.Chrome(service=Service())
+#     driver.maximize_window()
+#     return driver
 
 @pytest.fixture()
 def logger():
